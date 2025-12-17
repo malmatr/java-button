ddd221e379ad41ea87db0a6777dd40088000b9f9
malmatr <matraversmallory@gmail.com>
2025-12-16 18:50:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let button = document.getElementById("clicker");
 function clicked() {
   button.innerHTML = "CLICKED!";
 }
+`const clicker = document.getElementById("button");
+const counterDisplay = document.getElementById("counterDisplay");
+let counter = 0;
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.textContent = counter;
+});`
